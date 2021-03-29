29f1e116c84df450ca80bd5c82f5ed526236c8b1
yashar <yasharghaemi@gmail.com>
2021-03-29 15:12:44 -0400

    first commit

---
+++ tr_new_15/envSet.bat
@@ -0,0 +1,6 @@
+conda activate sumo
+SET _tx="C:\Program Files (x86)\Eclipse\Sumo\tools\traceExporter.py"
+SET _rt="C:\Program Files (x86)\Eclipse\Sumo\tools\randomTrips.py"
+
+echo "_rt : randomTrip.py"
+echo "_tx : traceExporter.py" 
